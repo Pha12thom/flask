d2d6b3c97536812ff93a2ba9c6d6d227f9ff64eb
pha12thom <MilugoGeofrey.Ouma@studentambassadors.com>
2024-04-11 12:15:39 +0300

    commit model

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datatime import datetime
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
 
 
 @app.route('/')
